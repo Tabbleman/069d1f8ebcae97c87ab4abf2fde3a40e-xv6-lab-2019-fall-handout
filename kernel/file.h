f6e39770948510303023ff4aed9b8117fa6de3d7
tabbleman <tabbleman11@gmail.com>
2023-11-11 11:46:25 +0800

    update fs

--- kernel/file.h
+++ kernel/file.h
@@ -26,7 +26,9 @@ struct inode {
   short minor;
   short nlink;
   uint size;
-  uint addrs[NDIRECT+1];
+  // 12 direct block and 256 indirect block 
+  // block number is 4 byte and a uint can represent 2^(32 / 4) = 256
+  uint addrs[NDIRECT+1];  
 };
 
 // map major device number to device functions.
